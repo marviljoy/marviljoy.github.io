bded46b5fdd6ead459998a885e3ddd3f9483d6c8
Marvil Joy <marvil.joy@gmail.com>
2013-08-20 22:01:44 +0530

    Mailing updated, index too

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@
 	$ip = $_SERVER['REMOTE_ADDR'];
 	
 //Qui andrà inserito il tuo indirizzo e-mail
-$to = "yourmail@xxx.com";
+$to = "marvil.joy@gmail.com";
 
 //Creazione del mesaggio da inviare
 $message = "New message from: ".$name.", ".$mail.".<br />";
@@ -15,7 +15,7 @@ $message .= "Text: <br />".$text."<br /><br />";
 $message .= "IP: ".$ip."<br />";
 $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-$headers .= "From: Your Curriculum Website"; 
+$headers .= "From: Marvil Joy Github"; 
 
 //Se l'e-mail viene spedita correttamente, compare un messaggio di avvenuto invio
  if(mail($to, $subject,$message, $headers)){
